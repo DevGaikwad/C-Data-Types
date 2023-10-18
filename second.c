0d508d46f91f151cb1eaa1609b9b8e5ba80f35f3
DevGaikwad <141038667+DevGaikwad@users.noreply.github.com>
2023-10-18 17:17:31 +0530

    Add files via upload

---
+++ second.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int age =55;
+    float call=88.90;
+    char text='c';
+    double amount = 200000;
+    printf("%d\n", age);
+    printf("%f\n", call);
+    printf("%c\n", text);
+    printf("%lf", amount);
+    return 0;
+}
